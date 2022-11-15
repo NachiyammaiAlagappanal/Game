f4a386de8f55590d9d48d30f542b36c04a2f546c
nvn-laufire <nvn@laufire.in>
2022-11-15 20:52:49 +0530

    stopped the movements of objects while touching the board.

--- src/core/actions.js
+++ src/core/actions.js
@@ -1,5 +1,6 @@
 import MovementManager from '../service/MovementManager';
 import ShapeManager from '../service/ShapeManager';
+// import WarpManager from '../service/WarpManager';
 
 const addShapes = (context) => ({
 	shapes: ShapeManager.addShape(context),
@@ -7,10 +8,14 @@ const addShapes = (context) => ({
 const manageMovement = (context) => ({
 	shapes: MovementManager.movingShapes(context),
 });
+// const warpManager = (context) => ({
+// 	shapes: WarpManager.movingShapes(context),
+// });
 
 const actions = {
 	addShapes,
 	manageMovement,
+	// warpManager,
 };
 
 export default actions;
--- src/service/MovementManager.js
+++ src/service/MovementManager.js
@@ -5,17 +5,42 @@ const directions = {
 	bottom: { x: 0, y: -1 },
 };
 
-const MovementManager = {
-	movingShapes: ({ state: { shapes }, config: { distance }}) =>
-		shapes.map((shape) => {
-			const direction = directions[shape.direction];
+const getXValue = (
+	shape, boardSize, half, distance
+) => {
+	const limit = shape.size * half;
+	const margin = boardSize - limit;
+	const direction = directions[shape.direction];
 
-			return {
-				...shape,
-				x: shape.x + (direction.x * distance),
-				y: shape.y + (direction.y * distance),
-			};
-		}),
+	return limit >= shape.x || shape.x >= margin
+		? shape.x
+		: shape.x + (direction.x * distance);
+};
+
+const getYValue = (
+	shape, boardSize, half, distance
+) => {
+	const limit = shape.size * half;
+	const margin = boardSize - limit;
+	const direction = directions[shape.direction];
+
+	return limit >= shape.y || shape.y >= margin
+		? shape.y
+		: shape.y + (direction.y * distance);
+};
+
+const MovementManager = {
+	movingShapes: ({ state: { shapes },
+		config: { distance, boardSize, half }}) =>
+		shapes.map((shape) => ({
+			...shape,
+			x: getXValue(
+				shape, boardSize, half, distance
+			),
+			y: getYValue(
+				shape, boardSize, half, distance
+			),
+		})),
 };
 
 export default MovementManager;
